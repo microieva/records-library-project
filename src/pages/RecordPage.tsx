828cf3e99e087cf49c31a69aa4f3829189a32054
Ieva Vyliaudaite <ievavyl@yahoo.com>
2021-03-13 15:47:44 +0200

    record-page scss

--- src/pages/RecordPage.tsx
+++ src/pages/RecordPage.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-//import { useParams } from 'react-router';
+import { useParams } from "react-router";
 //import { useHistory } from "react-router-dom";
 
 import { Record } from "../types";
@@ -10,18 +10,53 @@ type RecordPageProps = {
   records: Record[];
 };
 
+interface RouteParams {
+  title: string;
+}
+
 const RecordPage = (props: RecordPageProps) => {
   const { records } = props;
-  //const { title } = useParams()
+  const { title } = useParams<RouteParams>();
 
-  //let record = records.find(r => r.title === title)
   console.log("RECORD PAGE: ", records);
+  let record = records.find((r) => r.title === title);
   //const history = useHistory()
   /*const handleClick =()=>{
     history.push("/");
   }*/
+  console.log("RECORD: ", record);
 
-  return <div>record page</div>;
+  return (
+    <div className="record-page-container">
+      {record ? (
+        <div className="flex--column">
+          <div className="flex--row">
+            <div>
+              <img src={record.image} alt="record" />
+            </div>
+            <div>
+              <h1>{record.title}</h1>
+              <p>artist(s):</p>
+              <h3>{record.authors.map((artist) => artist).join(", ")}</h3>
+              <p>label:</p>
+              <h3>
+                {record.label}, {record.publishedYear}
+              </h3>
+              <p>genres:</p>
+              <h3>{record.genres}</h3>
+            </div>
+          </div>
+          <div className="tracks-div">
+            {record.tracks.map((track) => (
+              <p>{track.name}</p>
+            ))}
+          </div>
+        </div>
+      ) : (
+        false
+      )}
+    </div>
+  );
 };
 
 export default RecordPage;
